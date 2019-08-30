4788a686d5c30b519b68968ace771b4ed66dee3d
Martuza Shimul <martuza.shimul@gmail.com>
2019-08-30 21:38:52 +0600

    contact code upadet

--- contact_me.php
+++ contact_me.php
@@ -22,12 +22,13 @@ $message = $_POST['Message'];
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\n Phone: $phone \n\n City: $city \n\nMessage:\n$message";
-$headers = "From: Info@gas-engineer-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: Info@commercial-gas-engineer.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 
+// echo "<h4>The details have been sent and a member of staff will be in touch shortly!</h4>";
 // header("Location: index.html")	;
-header("Location: index.html");
+header("Location: thank-you.html");
 return true;	
 
 ?>
